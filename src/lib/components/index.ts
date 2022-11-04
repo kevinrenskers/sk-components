b313e89aeff1254a8cc3e3f5e4ab194cd50028f0
Kevin Renskers <kevin@loopwerk.io>
2022-11-04 23:53:51 +0100

    first commit

---
+++ src/lib/components/index.ts
@@ -0,0 +1,4 @@
+import Avatar from "./Avatar.svelte";
+import Button from "./Button.svelte";
+
+export { Avatar, Button };
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Button } from "$lib/components";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<Button />
